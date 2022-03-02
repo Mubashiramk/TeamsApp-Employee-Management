22ac89a2cc1ddbbce59ddddb28cb2937496e87b6
Mubashira M K <95755210+Mubashiramk@users.noreply.github.com>
2022-03-03 02:00:00 +0530

    Add files via upload

---
+++ Data/EmployeeDetailsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeDetails.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDetails.Data
+{
+    public class EmployeeDetailsDbContext : DbContext
+    {
+        public EmployeeDetailsDbContext(DbContextOptions<EmployeeDetailsDbContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<Employee> Applicants { get; set; }
+        
+    }
+}
+
