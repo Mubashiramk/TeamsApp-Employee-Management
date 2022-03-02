ff8686650d8324a4269a6a9db5abc0d6d2a60fc3
Mubashira M K <95755210+Mubashiramk@users.noreply.github.com>
2022-03-03 02:03:07 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace EmployeeDetails.Models
+{
+    public class Employee
+    {
+
+
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [StringLength(10)]
+        public string Gender { get; set; } = "";
+
+        [Required]
+        [StringLength(150)]
+        public string Project { get; set; } = "";
+
+        [Required]
+        [StringLength(50)]
+        public string Experience { get; set; } = "";
+    }
+}
